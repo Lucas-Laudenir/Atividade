28eb66ecd76a49e4d671016278f62ec58ddb7da6
lucas <lucaslaudenirfilipim@gmail.com>
2025-04-08 19:15:55 -0300

    atividade

---
+++ src/main/java/com/example/Avitidade/AvitidadeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Avitidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvitidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvitidadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Avitidade/AvitidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Avitidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvitidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
